847613c9ef02077d51e1f0266268cabdaf27bfc6
otomatty <saedgewell@gmail.com>
2026-01-31 13:19:22 +0900

    feat: add key prop to PageEditorView for state reset on page transitions

--- src/pages/PageEditor.tsx
+++ src/pages/PageEditor.tsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
 import PageEditorView from '@/components/editor/PageEditorView';
 
 const PageEditorPage = () => {
-  return <PageEditorView />;
+  const { id } = useParams<{ id: string }>();
+  
+  // idをkeyとして設定することで、ページ遷移時（WikiLinkクリック等）に
+  // PageEditorViewが完全に再マウントされ、すべての状態がリセットされる
+  return <PageEditorView key={id} />;
 };
 
 export default PageEditorPage;
